81ea75d4b6885ef3abb07a7a873ea222eaccda0a
liu-adalet <liu@adaletgpt.com>
2024-03-28 16:21:21 -0400

    feat(update): update config for database

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "AdaletGPT"
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    SQLALCHEMY_DATABASE_URI: str = "postgresql://postgres:postgres@localhost/adaletgpt"
+    SQLALCHEMY_DATABASE_URI: str = "postgresql://postgres:adminli0220@localhost/adaletgpt"
     OPENAI_API_KEY: str = "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"
     PINECONE_API_KEY: str = "c214371b-cf98-4c07-8afc-95a3623a518d"
     INDEX_NAME: str = "adaletgpt-large-embedding"
