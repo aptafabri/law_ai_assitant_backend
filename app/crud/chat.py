c85fd319d797ce26855c168f5415772fd0b4778d
aptafabri <aptafabri@gmail.com>
2024-04-01 17:35:18 -0400

    feat(update): update sql query

--- app/crud/chat.py
+++ app/crud/chat.py
@@ -3,36 +3,45 @@ from database.session import get_session
 from schemas.message import ChatAdd, SessionSummary, Message
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import func
+from sqlalchemy import select, text
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import delete
 from typing import List
 
 def get_sessions_by_userid(user_id: int, session: Session) -> List[SessionSummary]:
     
-    session_ids= session.query(ChatHistory.session_id).filter(ChatHistory.user_id == user_id).distinct().all()
+    query = f"""
+        SELECT *
+        FROM (
+            SELECT *,
+                   ROW_NUMBER() OVER (PARTITION BY session_id ORDER BY id) AS row_num
+            FROM public.chat_history
+            WHERE session_id IN (
+                SELECT DISTINCT ON (session_id) session_id
+                FROM public.chat_history
+                WHERE user_id = {user_id}
+            )
+        ) AS subquery
+        WHERE row_num <= 2 ORDER BY created_date DESC;
+    """
+    results =session.execute(text(query)).fetchall()
     session_summary_array : List[SessionSummary] = []
-
-    for session_id, in session_ids[::-1]:
-        session_messages = session.query(ChatHistory).\
-            filter(ChatHistory.session_id == session_id).\
-            order_by(ChatHistory.created_date.asc()).order_by(ChatHistory.id.asc()).limit(2).all()
-        
-        for session_message in session_messages:
-            if(session_message.role == 'user'):
-                session_name = session_message.content
-            else:
-                session_summary = session_message.content
-
-        session_summary = SessionSummary(
-                session_id=session_id,
-                name=session_name,
-                summary=session_summary
-        )
-        
-        session_summary_array.append(session_summary)
+    prev_element = None
+    for index, result in enumerate(results):
+        if prev_element is not None:
+            if(index%2==1): 
+                session_summary = SessionSummary(
+                    session_id=result[2],
+                    name=prev_element[3],
+                    summary=result[3]
+                )
+                
+                session_summary_array.append(session_summary)
+        prev_element = result
     
-    print(session_summary_array)
     return session_summary_array
+         
+    
         
 def get_messages_by_session_id(user_id:int, session_id:str, session: Session)->List[Message]:
     
