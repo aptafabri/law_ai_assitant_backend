7f210cc559e0896dec6ad8c3e31bc83d58ebe07d
liu-adalet <liu@adaletgpt.com>
2024-04-05 08:03:58 -0400

    feat(update):update order of session list

--- app/crud/chat.py
+++ app/crud/chat.py
@@ -18,7 +18,7 @@ def get_sessions_by_userid(user_id: int, session: Session) -> List[SessionSummar
     session_summary_array : List[SessionSummary] = []
     results = session.query(SessionsummaryTable)\
         .filter(SessionsummaryTable.user_id == user_id)\
-        .order_by(SessionsummaryTable.created_date.asc()).all()
+        .order_by(SessionsummaryTable.created_date.desc()).all()
     session_summary_array = results
     return session_summary_array
 
