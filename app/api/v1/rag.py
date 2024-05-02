c0f615d357759dc243985fbfcbc416c9d8ec0397
liu-adalet <163865311+liu-adalet@users.noreply.github.com>
2024-05-02 10:33:58 +0000

    feat(update): update for bug fixing

--- app/api/v1/rag.py
+++ app/api/v1/rag.py
@@ -58,67 +58,38 @@ def chat_with_document(message:ChatRequest, dependencies=Depends(JWTBearer()), s
         )
 
 @router.post('/chat-legal', tags = ['RagController'], status_code = 200 )
-def chat_with_legal(session_id:str = Form(), question:str= Form(), file:UploadFile = File(...), dependencies = Depends(JWTBearer()),  session: Session = Depends(get_session)):
-    legal_question = ""
-    legal_s3_key = ""
-    file_name = ""
-    attached_pdf = False
+def chat_with_legal(message:ChatRequest, dependencies=Depends(JWTBearer()), session: Session = Depends(get_session)):
     user_id = get_userid_by_token(dependencies)
     created_date = datetime.now()
-    pdf_contents = file.read()
-    if pdf_contents:
-        attached_pdf = True
-        file_name = file.filename 
-        time_stamp = created_date.timestamp()
-        legal_s3_key = f"{time_stamp}_{file_name}"
-        upload_legal_description(file_content=pdf_contents, user_id = user_id, session_id= session_id, legal_s3_key= legal_s3_key)
-        legal_question = read_pdf(pdf_contents)
-    total_question = legal_question +  question
-    response = rag_legal_chat(question=total_question, session_id= session_id)
-    answer = response["answer"]
-    # user_message = LegalChatAdd(
-    #     user_id= user_id,
-    #     session_id= session_id,
-    #     content=question,
-    #     role= 'user',
-    #     legal_attached=attached_pdf,
-    #     legal_file_name=file_name,
-    #     legal_s3_key= legal_s3_key,
-    #     created_date = created_date
-    # )
-    # ai_message = LegalChatAdd(
-    #     user_id= user_id,
-    #     session_id= session_id,
-    #     content= answer,
-    #     role= 'assistant',
-    #     legal_attached=False,
-    #     legal_file_name='',
-    #     legal_s3_key= '',
-    #     created_date = created_date
-    # )
-    # add_legal_chat_message(user_message, session)
-    # add_legal_chat_message(ai_message, session)
 
-    print("total-question:", total_question)
-    if(legal_session_exist(session_id=session_id, session= session)==True):
+    response = rag_legal_chat(question=message.question, session_id= message.session_id)
+
+    user_message = ChatAdd( user_id = user_id, session_id= message.session_id, content= message.question, role = "user", created_date=created_date)
+    ai_message = ChatAdd( user_id = user_id, session_id= message.session_id, content= response["answer"], role = "assistant", created_date= created_date)
+    
+    add_legal_message(user_message, session)
+    add_legal_message(ai_message, session)
+    
+    print(legal_session_exist(session_id=message.session_id, session= session))
+    if(legal_session_exist(session_id=message.session_id, session= session)==True):
         return JSONResponse(
             content={
                 "user_id": user_id,
-                "session_id": session_id,
-                "question": question,
-                "answer":answer,
+                "session_id": message.session_id,
+                "question":message.question,
+                "answer":response["answer"],
             },
             status_code= 200
         )
     else:
-        summary = summarize_session(question=total_question, answer= answer)
-        add_legal_session_summary(user_id=user_id, session_id= session_id, summary= summary, session=session)
+        summary = summarize_session(question=message.question, answer= response["answer"])
+        add_legal_session_summary(user_id=user_id, session_id= message.session_id, summary= summary, session=session)
         return JSONResponse(
             content={
                 "user_id": user_id,
-                "session_id": session_id,
-                "question": question,
-                "answer":answer,
+                "session_id": message.session_id,
+                "question":message.question,
+                "answer":response["answer"],
                 "title":summary,
             },
             status_code= 200
@@ -205,7 +176,9 @@ async def rag_test(session_id:str = Form(), question:str= Form(), file:UploadFil
     #     )
 
     return {"ocr_text":total_question}
-   
+    # response = rag_test_chat(question=message.question, session_id= message.session_id)
+    
+    # return response
     
 
 
